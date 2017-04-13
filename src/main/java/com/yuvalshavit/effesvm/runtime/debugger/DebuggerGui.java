18784c0dd854c882ef7a3072d8a7a0e82fdab623
Yuval Shavit <yankee.sierra@gmail.com>
2017-04-13 01:46:40 -0400

    nicer port-field editing
    Also, remember the port when you close the window. With this, I can now
    just start the debugger once and keep reusing it.

--- src/main/java/com/yuvalshavit/effesvm/runtime/debugger/DebuggerGui.java
+++ src/main/java/com/yuvalshavit/effesvm/runtime/debugger/DebuggerGui.java
@@ -6,6 +6,8 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.EOFException;
@@ -16,6 +18,7 @@ import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Consumer;
@@ -23,6 +26,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
 
 public class DebuggerGui {
 
@@ -33,38 +40,75 @@ public class DebuggerGui {
   };
 
   public static void createConnectDialogue() {
+    createConnectDialogue(-1);
+  }
+
+  private static void createConnectDialogue(int initialPort) {
+    if (initialPort <= 0) {
+      initialPort = 6166;
+    }
     JFrame connectionWindow = new JFrame("EffesVM Debugger");
     connectionWindow.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
     Container content = connectionWindow.getContentPane();
 
     content.add(new JLabel("Connect by port"), BorderLayout.WEST);
-    JTextField portField = new JTextField(16);
-    portField.addActionListener(action -> tryConnect(action.getActionCommand(), connectionWindow));
-    content.add(portField, BorderLayout.EAST);
+
+    JFormattedTextField portTextField = new JFormattedTextField(new DecimalFormat("#####"));
+    portTextField.setColumns(5);
+    portTextField.setText(Integer.toString(initialPort));
+    ((PlainDocument) portTextField.getDocument()).setDocumentFilter(new DocumentFilter(){
+      @Override
+      public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+        if (isAllDigits(string)) {
+          super.insertString(fb, offset, string, attr);
+        }
+      }
+
+      @Override
+      public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+        if (isAllDigits(text)) {
+          super.replace(fb, offset, length, text, attrs);
+        }
+      }
+
+      private boolean isAllDigits(String s) {
+        return s.chars().allMatch((c) -> Character.isDigit((char)c));
+      }
+    });
+    Object enterAction = new Object();
+    portTextField.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), enterAction);
+    portTextField.getActionMap().put(enterAction, new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        tryConnect(portTextField.getText(), connectionWindow);
+      }
+    });
+    content.add(portTextField, BorderLayout.EAST);
     JButton connectButton = new JButton("Connection");
-    connectButton.addActionListener(action -> tryConnect(portField.getText(), connectionWindow));
+    connectButton.addActionListener(action -> tryConnect(portTextField.getText(), connectionWindow));
     content.add(connectButton, BorderLayout.SOUTH);
 
     connectionWindow.setLocationRelativeTo(null);
     connectionWindow.pack();
     connectionWindow.setVisible(true);
+    // due to silliness, we have to wait a bit before we can select-all
+    try {
+      Thread.sleep(30);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+    SwingUtilities.invokeLater(portTextField::selectAll);
   }
 
   private static void tryConnect(String portString, JFrame connectionWindow) {
-    int portInt;
     try {
-      portInt = Integer.parseInt(portString);
-    } catch (NumberFormatException e) {
-      return;
-    }
-    try {
-      DebugConnection debugConnection = new DebugConnection(portInt);
+      DebugConnection debugConnection = new DebugConnection(Integer.parseInt(portString));
       debugConnection.communicate(new MsgHello(), r -> {
         connectionWindow.dispose();
         createDebugWindow(debugConnection);
       });
-    } catch (IOException e) {
+    } catch (NumberFormatException | IOException e) {
       e.printStackTrace();
     }
   }
@@ -135,6 +179,7 @@ public class DebuggerGui {
           } catch (IOException e) {
             throw new RuntimeException(e);
           }
+          createConnectDialogue(connection.port);
         }
       });
 
