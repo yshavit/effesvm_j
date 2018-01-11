6653896c5091ff30509230fe9afa17e0682f5274
Yuval Shavit <yankee.sierra@gmail.com>
2018-01-11 01:17:03 -0500

    add tryGetFromEvmType

--- effesvm_j-common/src/main/java/com/yuvalshavit/effesvm/runtime/EffesNativeType.java
+++ effesvm_j-common/src/main/java/com/yuvalshavit/effesvm/runtime/EffesNativeType.java
@@ -1,5 +1,8 @@
 package com.yuvalshavit.effesvm.runtime;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum EffesNativeType {
   TRUE("True"),
   FALSE("False"),
@@ -9,8 +12,17 @@ public enum EffesNativeType {
   ARRAY("Array"),
   STRING_BUILDER("StringBuilder"),
   STREAM_IN("StreamIn"),
-  STREAM_OUT("StreamOut"),
-  ;
+  STREAM_OUT("StreamOut"),;
+
+  private static final Map<String, EffesNativeType> byEvmType;
+
+  static {
+    EffesNativeType[] values = EffesNativeType.values();
+    byEvmType = new HashMap<>(values.length);
+    for (EffesNativeType value : values) {
+      byEvmType.put(value.evmType, value);
+    }
+  }
 
   private final String evmType;
 
@@ -20,5 +32,9 @@ public enum EffesNativeType {
 
   public String getEvmType() {
     return evmType;
+  }
+
+  public static EffesNativeType tryGetFromEvmType(String type) {
+    return byEvmType.get(type);
   }
 }
