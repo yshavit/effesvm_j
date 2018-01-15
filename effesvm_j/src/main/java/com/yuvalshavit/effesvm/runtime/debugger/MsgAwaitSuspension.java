c9906dabb64ff81d785dfda5007584b491a732a9
Yuval Shavit <yankee.sierra@gmail.com>
2018-01-15 17:41:26 -0500

    remove unused classes

--- effesvm_j/src/main/java/com/yuvalshavit/effesvm/runtime/debugger/MsgAwaitSuspension.java
+++
@@ -1,9 +0,0 @@
-package com.yuvalshavit.effesvm.runtime.debugger;
-
-public class MsgAwaitSuspension extends Msg.NoResponse {
-
-  @Override
-  void run(DebuggerState state) throws InterruptedException {
-    state.awaitSuspension();
-  }
-}
